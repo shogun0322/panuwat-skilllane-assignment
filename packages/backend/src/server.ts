9d22730ee5f6f1b8b3a5f58a5abca7fef27a4777
Panuwat <shogun0322@hotmail.com>
2025-06-24 19:48:20 +0700

    Create be

---
+++ packages/backend/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3001 });
+    console.log("Server running at http://localhost:3001");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
